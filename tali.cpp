ae8c21c48865c073909c2a6611f62c912aa1800e
talib.cpp <talibffdc@gmail.com>
2021-10-22 17:47:26 -0700

    new

---
+++ tali.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=56;
+    int b=78;
+    int c=58;
+
+    if(a>b) 
+    {
+        if(a>c)
+        {
+            cout<<"A is Greater";
+        }
+    }
+else
+{
+    if(b>c)
+    {
+
+    cout<<"b is the Greatest";
+    }
+    else
+    {
+        cout<<"c is Greatest";
+    }
+}
+
+return 0;
+}
