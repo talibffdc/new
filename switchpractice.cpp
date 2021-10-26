ab6c06a33a70c2fa0dbc34dc6c93c15b1715db0a
talib.cpp <talibffdc@gmail.com>
2021-10-25 23:53:54 -0700

    switch programm

---
+++ switchpractice.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+        int a=3;
+
+        switch(a)
+        {
+
+        case 1:
+            cout<<" 1 is stored in a ";
+        break;
+            case 2:
+        cout<< "2 is stored in a ";
+        break;
+
+            case 3:
+
+        cout<< " 3 is stored in a ";
+        break;
+                case 4:
+        cout<< "4 is stored in a";
+            break;
+            default:
+        cout << "value is not in there ";
+            break;
+        }
+        return 0;
+}
